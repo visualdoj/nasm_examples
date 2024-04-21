26bd481421570bfa4ef478d3cb54abf3baf647a3
Doj <vitek03@gmail.com>
2024-04-21 02:11:02 +0000

    fix test for program exit77 on linux

--- test/test_examples.py
+++ test/test_examples.py
@@ -5,12 +5,12 @@ def test_exit77():
 	BIN = os.environ['BIN']
 	SRC = os.environ['SRC']
 	EXEEXT = os.environ['EXEEXT']
-	if 'hello.asm' not in os.listdir(SRC):
+	if 'exit77.asm' not in os.listdir(SRC):
 		return
 
 	exit77_exe = os.path.join(BIN, 'exit77' + EXEEXT)
 	p = subprocess.run(exit77_exe, shell=True, capture_output=True)
-	assert p.returncode == 77, 'exit77.exe should return 0 exit code'
+	assert p.returncode == 77, 'exit77.exe should return exit code 77'
 	assert p.stdout.decode("utf-8") == ''
 	assert p.stderr.decode("utf-8") == ''
 
@@ -23,6 +23,6 @@ def test_hello():
 
 	hello_exe = os.path.join(BIN, 'hello' + EXEEXT)
 	p = subprocess.run(hello_exe, shell=True, capture_output=True)
-	assert p.returncode == 0, 'hello.exe should return 0 exit code'
+	assert p.returncode == 0, 'hello.exe should exit successfully'
 	assert p.stdout.decode("utf-8").strip() == 'Hello world!'
 	assert p.stderr.decode("utf-8") == ''
